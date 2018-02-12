829be5d3dddf8d26454becbae1f7bf68a4d835b1
Brian Whitton <me@brianwhitton.com>
2018-02-12 18:54:27 -0500

    bind is-crawling class

--- app/components/track-info/component.js
+++ app/components/track-info/component.js
@@ -1,5 +1,6 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-  classNames: ['track-info']
+  classNames: ['track-info'],
+  classNameBindings: ['watch:is-crawling']
 });
