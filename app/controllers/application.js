a560e3f541e47bc54c2d6f97d81de3a85bee80a7
Brian Whitton <me@brianwhitton.com>
2018-02-12 18:55:13 -0500

    programmatically determine whether to use a crawl or not based on MQ

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,8 +1,26 @@
 import Controller from '@ember/controller';
+import { bind } from '@ember/runloop';
 
 export default Controller.extend({
   queryParams: ['title', 'blurb', 'stories'],
   title: null,
   blurb: null,
-  stories: null
+  stories: null,
+
+  init() {
+    this._super(...arguments);
+    let media = window.matchMedia("(max-width: 767px)");
+    this.set('media', media);
+    this.set('boundListener', bind(this, 'setTextCrawl'));
+    media.addListener(this.get('boundListener'));
+  },
+
+  willDestroyElement() {
+    this._super(...arguments);
+    this.get('media').removeListener(this.get('boundListener'));
+  },
+
+  setTextCrawl(mql) {
+    this.set('crawl', mql.matches);
+  }
 });
