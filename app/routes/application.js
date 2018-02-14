8fee11d891f1720eef3966c0e149deb0c54673e9
Brian Whitton <me@brianwhitton.com>
2018-02-14 17:46:27 -0500

    refresh if stories qp changes

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,6 +2,11 @@ import Route from '@ember/routing/route';
 import RSVP from 'rsvp';
 
 export default Route.extend({
+  queryParams: {
+    stories: {
+      refreshModel: true
+    }
+  },
   model({ stories }) {
     let slugs = stories.split(',').filter(Boolean);
     let requests = slugs.map(slug => this.store.findRecord('story', slug));
