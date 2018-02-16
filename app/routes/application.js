b2b454339636e5af6fe9126115bb266516bf7b51
Brian Whitton <me@brianwhitton.com>
2018-02-16 12:06:52 -0500

    gracefully handle 404 responses

--- app/routes/application.js
+++ app/routes/application.js
@@ -12,5 +12,18 @@ export default Route.extend({
     let requests = slugs.map(slug => this.store.findRecord('story', slug));
 
     return RSVP.Promise.all(requests);
+  },
+  actions: {
+    error(e) {
+      if (e && e.errors) {
+        if (e.errors[0].status === '404') {
+          return;
+        } else {
+          throw e;
+        }
+      } else {
+        throw e;
+      }
+    }
   }
 });
