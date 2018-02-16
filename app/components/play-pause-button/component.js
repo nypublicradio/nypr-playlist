205df824b3e5bdc21afecdcaad1525b8fc26e35a
Brian Whitton <me@brianwhitton.com>
2018-02-16 18:08:07 -0500

    add theme styles to various spots

--- app/components/play-pause-button/component.js
+++ app/components/play-pause-button/component.js
@@ -1,7 +1,13 @@
 import Component from '@ember/component';
+import { htmlSafe } from '@ember/string';
+import { computed } from '@ember/object';
 
 export default Component.extend({
   tagName: '',
+
+  buttonColor: computed('styles', function() {
+    return htmlSafe(`color: ${this.get('styles.accent.color')}`)
+  }),
   actions: {
     toggle() {
       if (this.get('isPlaying')) {
