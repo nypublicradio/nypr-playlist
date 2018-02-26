16e65be96a229e233924570e4efb521e90eac6d7
Brian Whitton <me@brianwhitton.com>
2018-02-26 17:53:18 -0500

    update analytics acceptance test

--- app/routes/application.js
+++ app/routes/application.js
@@ -13,7 +13,7 @@ export default Route.extend({
     let requests = slugs.map(slug => this.store.findRecord('story', slug));
 
     if (window.dataLayer) {
-      window.dataLayer.push({playlistTitle: title});
+      window.dataLayer.push({playlistTitle: title, gaCategory: 'Playlist Widget'});
     }
 
     return RSVP.Promise.all(requests);
