b97914d73667f27499ff66858e51336f0d23adb8
Brian Whitton <me@brianwhitton.com>
2018-02-27 08:59:50 -0500

    actually, it's better to run things via hifi events
    allows us to hook into actions triggered by the player itsself

--- app/components/nypr-playlist/component.js
+++ app/components/nypr-playlist/component.js
@@ -2,7 +2,7 @@ import Component from '@ember/component';
 import { reads } from '@ember/object/computed';
 import { inject as service } from '@ember/service';
 import { get, set } from '@ember/object';
-import { run } from '@ember/runloop';
+import { run, bind } from '@ember/runloop';
 import { sum, mapBy } from '@ember/object/computed';
 
 export default Component.extend({
@@ -17,13 +17,14 @@ export default Component.extend({
   init() {
     this._super(...arguments);
     get(this, 'hifi').on('audio-ended', () => run(this, 'queueUp'));
+    get(this, 'hifi').on('audio-played', bind(this, 'analytics'));
   },
 
-  play(item) {
+  play(item, event) {
     set(this, 'showPlayer', true);
 
     let audio = get(item, 'audio');
-    return get(this, 'hifi').play(audio, {metadata: {item}});
+    return get(this, 'hifi').play(audio, {metadata: {item, event}});
   },
 
   pause() {
@@ -33,44 +34,40 @@ export default Component.extend({
   queueUp() {
     let currentItem = get(this, 'currentItem');
     let currentIndex = get(this, 'items').indexOf(currentItem);
+    let nextItem = get(this, 'items').objectAt(currentIndex + 1);
+
     if (currentIndex === (get(this, 'items.length') - 1)) {
       set(this, 'showPlayer', false);
     } else {
-      let nextItem = get(this, 'items').objectAt(currentIndex + 1);
-      this.play(nextItem).then(() => {
-        if (window.dataLayer) {
-          window.dataLayer.push({event: 'playlist-passiveStart', 'playlist-currentStory': get(nextItem, 'title'), 'playlist-currentShow': get(nextItem, 'showTitle')});
-        }
-      });
+      this.play(nextItem, 'playlist-passiveStart');
     }
   },
 
   playAll() {
     let firstItem = get(this, 'items.firstObject');
-    this.play(firstItem).then(() => {
-      if (window.dataLayer) {
-        window.dataLayer.push({event: 'playlist-passiveStart', 'playlist-currentStory': get(firstItem, 'title'), 'playlist-currentShow': get(firstItem, 'showTitle')});
-      }
-    });
+    this.play(firstItem, 'playlist-passiveStart');
   },
 
-  playItemFromList(item) {
-    this.play(item).then(({sound}) => {
-      let event;
+  analytics(sound) {
+    if (!window.dataLayer) {
+      return;
+    }
+    let { event, item } = sound.get('metadata');
+    let currentIndex = get(this, 'items').indexOf(item);
+
+    if (!event) {
       if (Math.floor(sound.get('position')) === 0) {
         event = 'playlist-start';
       } else {
         event = 'playlist-resume';
       }
-      if (window.dataLayer) {
-        let currentIndex = get(this, 'items').indexOf(item);
-        window.dataLayer.push({
-          event,
-          'playlist-currentPosition': currentIndex + 1,
-          'playlist-currentStory': get(item, 'title'),
-          'playlist-currentShow': get(item, 'showTitle')
-        });
-      }
+    }
+    window.dataLayer.push({
+      event,
+      'playlist-currentPosition': currentIndex + 1,
+      'playlist-currentStory': get(item, 'title'),
+      'playlist-currentShow': get(item, 'showTitle')
     });
+
   }
 });
